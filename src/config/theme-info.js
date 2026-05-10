08ee5b164f4556f748d76099570b1eecee286989
Platinum <noreply@github.com>
2026-05-10 16:30:24 +0800

    Fix SillyBunny topbar spacing

--- src/config/theme-info.js
+++ src/config/theme-info.js
@@ -1,5 +1,5 @@
 export const EXTENSION_NAME = 'Moonlit Echoes Theme (SillyBunny)';
 export const EXTENSION_ID = 'SillyBunny-MoonlitEchoesTheme';
 export const EXTENSION_FOLDER_PATH = `scripts/extensions/third-party/${EXTENSION_ID}`;
-export const THEME_VERSION = '3.1.1-sb2';
+export const THEME_VERSION = '3.1.1-sb3';
 export const EXTENSION_REPOSITORY_URL = 'https://github.com/platberlitz/SillyBunny-MoonlitEchoesTheme';
