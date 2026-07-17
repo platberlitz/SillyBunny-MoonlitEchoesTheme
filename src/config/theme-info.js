338253d92702e6edaa4df44d8736ec57cd229f6f
Platinum <noreply@github.com>
2026-07-17 09:38:17 +0800

    Size drawer avatar images explicitly for WebKit

--- src/config/theme-info.js
+++ src/config/theme-info.js
@@ -1,5 +1,5 @@
 export const EXTENSION_NAME = 'Moonlit Echoes Theme (SillyBunny)';
 export const EXTENSION_ID = 'SillyBunny-MoonlitEchoesTheme';
 export const EXTENSION_FOLDER_PATH = `scripts/extensions/third-party/${EXTENSION_ID}`;
-export const THEME_VERSION = '3.2.0-sb7';
+export const THEME_VERSION = '3.2.0-sb8';
 export const EXTENSION_REPOSITORY_URL = 'https://github.com/platberlitz/SillyBunny-MoonlitEchoesTheme';
